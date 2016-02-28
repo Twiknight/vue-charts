dd64588f886f4daa253a7bb05f0b4ebfe59e99f1
Twiknight <twiknight@163.com>
2016-02-28 09:51:41 +0800

    Add temporary svg outerHTML polyfill for IE and Edge

--- src/main.js
+++ src/main.js
@@ -5,6 +5,17 @@ import {
 }
 from 'd3'
 
+//outerHTML polyfill  for SVGElements
+//should be remove when update Vue to 1.0.17 releases
+Object.defineProperty(SVGElement.prototype, 'outerHTML', {
+  get: function() {
+    let wrapper = document.createElement('div')
+    wrapper.appendChild(this.cloneNode(true))
+    return wrapper.innerHTML
+  }
+})
+
+
 new Vue({
   el: 'body',
   components: {
