3ffe110ab6f163e0c9af1f49f4710a1ae30c3908
Twiknight <twiknight@163.com>
2016-02-29 19:01:41 +0800

    update vue to 1.0.17 and remove outerhtml polyfill

--- src/main.js
+++ src/main.js
@@ -5,17 +5,6 @@ import {
 }
 from 'd3'
 
-//outerHTML polyfill  for SVGElements
-//should be remove when update Vue to 1.0.17 releases
-Object.defineProperty(SVGElement.prototype, 'outerHTML', {
-  get: function() {
-    let wrapper = document.createElement('div')
-    wrapper.appendChild(this.cloneNode(true))
-    return wrapper.innerHTML
-  }
-})
-
-
 new Vue({
   el: 'body',
   components: {
